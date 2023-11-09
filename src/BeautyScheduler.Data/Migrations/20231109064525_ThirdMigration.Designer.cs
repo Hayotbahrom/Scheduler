2a7e3ba04a57fdc5065436b0ce7286fc78e4c124
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2023-11-09 11:50:04 +0500

    done nearly

--- src/BeautyScheduler.Data/Migrations/20231109064525_ThirdMigration.Designer.cs
+++ src/BeautyScheduler.Data/Migrations/20231109064525_ThirdMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BeautyScheduler.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BeautyScheduler.Data.Migrations
 {
     [DbContext(typeof(BeautySchedulerDbContext))]
-    partial class BeautySchedulerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231109064525_ThirdMigration")]
+    partial class ThirdMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/BeautyScheduler.Data/Migrations/20231109064525_ThirdMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeautyScheduler.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThirdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IMAGE",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IMAGE",
+                table: "Customers");
+        }
+    }
+}
